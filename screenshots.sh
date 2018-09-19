041c077d6c044956544fabbac67156b64d4be149
Alexey Pobery <poberyaa@gmail.com>
2018-09-20 00:03:00 +0300

    Check args count in 'screenshot' script

--- screenshots.sh
+++ screenshots.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-ffmpeg -i $1 -vf fps=1/$2 out%d.png
+if [[ $# < 2 ]]; then
+	echo "Provide two parameters: input file and seconds shift for screenshot"
+	exit 2
+fi
+
+INPUT_FILE=$1
+SECONDS=$2
+
+ffmpeg -i $INPUT_FILE -vf fps=1/$SECONDS %d.png
