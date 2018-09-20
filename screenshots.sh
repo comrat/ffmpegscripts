2beb00e92109298ef79e106c16e15a5978052eaa
Alexey Pobery <poberyaa@gmail.com>
2018-09-20 10:43:03 +0300

    Provide default values + add pass output dir as third argument

--- screenshots.sh
+++ screenshots.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
 
-if [[ $# < 2 ]]; then
-	echo "Provide two parameters: input file and seconds shift for screenshot"
-	exit 2
+if [[ $# < 1 ]]; then
+	echo "Provide input file"
+	exit 1
 fi
 
 INPUT_FILE=$1
-SECONDS=$2
+OUTPUT_DIR="."
+SECONDS=10
 
-ffmpeg -i $INPUT_FILE -vf fps=1/$SECONDS %d.png
+if [[ $# > 1 ]]; then
+	SECONDS=$2
+fi
+
+if [[ $# > 2 ]]; then
+	OUTPUT_DIR=$3
+fi
+
+ffmpeg -i $INPUT_FILE -vf fps=1/$SECONDS "$OUTPUT_DIR/%d.png"
