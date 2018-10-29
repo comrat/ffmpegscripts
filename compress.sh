3a632ce80a294f472a0c6bcfdd359ce61da8d9a1
Alexey Pobery <poberyaa@gmail.com>
2018-10-30 02:29:56 +0300

    Add script to compress single file

---
+++ compress.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+if [[ $# < 1 ]]; then
+	echo "Provide at least one parameter: input file"
+	exit 2
+fi
+
+INPUT_FILE=$1
+OUTPUT_FILE="output.mp4"
+
+if [[ $# > 1 ]]; then
+	OUTPUT=$2
+fi
+
+ffmpeg -i "$INPUT_FILE" -c:v libx264 -s 1280x720 -acodec mp3 -f mp4 "$OUTPUT_FILE"
