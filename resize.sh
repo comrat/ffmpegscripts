8c9e22b2f5f976538ad09f5e090ee7f4f3b998b1
Alexey Pobery <poberyaa@gmail.com>
2019-01-31 22:54:10 +0300

    Add resize script

---
+++ resize.sh
@@ -0,0 +1,37 @@
+#! /bin/bash
+
+POSITIONAL=()
+while [[ $# -gt 0 ]]
+do
+key="$1"
+
+case $key in
+    -i|--input)
+    INPUT_FILE="$2"
+    shift # past argument
+    shift # past value
+    ;;
+    -o|--output)
+    OUTPUT_FILE="$2"
+    shift # past argument
+    shift # past value
+    ;;
+    -w|--width)
+    WIDTH="$2"
+    shift # past argument
+    shift # past value
+    ;;
+    -h|--height)
+    HEIGHT="$2"
+    shift # past argument
+    shift # past value
+    ;;
+    *)    # unknown option
+    POSITIONAL+=("$1") # save it in an array for later
+    shift # past argument
+    ;;
+esac
+done
+set -- "${POSITIONAL[@]}" # restore positional parameters
+
+ffmpeg -i $INPUT_FILE -vf scale=$WIDTH:$HEIGHT $OUTPUT_FILE
