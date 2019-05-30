da7abfe0465fe9a33cf1a1a6b2858a8a527b7a67
Alexey Pobery <poberyaa@gmail.com>
2019-05-30 23:28:40 +0300

    Add watermark script

---
+++ watermark.sh
@@ -0,0 +1,15 @@
+if [[ $# < 1 ]]; then
+	echo "Provide at least one parameter: input file"
+	exit 2
+fi
+
+INPUT_FILE=$1
+OUTPUT_FILE="output.mp4"
+
+if [[ $# > 1 ]]; then
+	OUTPUT_FILE=$2
+fi
+
+WATERMARK="watermark.png"
+
+ffmpeg -i $INPUT_FILE -i $WATERMARK -filter_complex "overlay=main_w-overlay_w-20:10" $OUTPUT_FILE
