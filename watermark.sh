82be6b3a20519ee2ed828fff6c678a4d6566d0d5
Alexey Pobery <poberyaa@gmail.com>
2019-06-04 23:07:41 +0300

    Pass watermark as third argument

--- watermark.sh
+++ watermark.sh
@@ -5,11 +5,14 @@ fi
 
 INPUT_FILE=$1
 OUTPUT_FILE="output.mp4"
+WATERMARK="watermark.png"
 
 if [[ $# > 1 ]]; then
 	OUTPUT_FILE=$2
 fi
 
-WATERMARK="watermark.png"
+if [[ $# > 2 ]]; then
+	WATERMARK=$3
+fi
 
 ffmpeg -i $INPUT_FILE -i $WATERMARK -filter_complex "overlay=main_w-overlay_w-20:10" $OUTPUT_FILE
