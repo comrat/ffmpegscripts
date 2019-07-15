7e4870b4d4087c0550726e6a2767ffa153e1d669
Alexey Pobery <poberyaa@gmail.com>
2019-07-16 01:27:36 +0300

    Add script for audio removing

---
+++ remove_audio.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+if [[ $# < 1 ]]; then
+	echo "Provide at least one parameter: input file"
+	exit 2
+fi
+
+INPUT_FILE=$1
+OUTPUT_FILE="output.mp4"
+
+if [[ $# > 1 ]]; then
+	OUTPUT_FILE=$2
+fi
+
+ffmpeg -i $INPUT_FILE -vcodec copy -an $OUTPUT_FILE
