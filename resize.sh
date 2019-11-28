71717e1b10aae069d7d6975bbe5bae02b0ef4d70
Alexey Pobery <poberyaa@gmail.com>
2019-11-28 23:53:51 +0300

    Add default size value + fix format

--- resize.sh
+++ resize.sh
@@ -1,36 +1,38 @@
 #! /bin/bash
 
 POSITIONAL=()
+WIDTH=100
+HEIGHT=100
+
 while [[ $# -gt 0 ]]
 do
-key="$1"
-
-case $key in
-    -i|--input)
-    INPUT_FILE="$2"
-    shift # past argument
-    shift # past value
-    ;;
-    -o|--output)
-    OUTPUT_FILE="$2"
-    shift # past argument
-    shift # past value
-    ;;
-    -w|--width)
-    WIDTH="$2"
-    shift # past argument
-    shift # past value
-    ;;
-    -h|--height)
-    HEIGHT="$2"
-    shift # past argument
-    shift # past value
-    ;;
-    *)    # unknown option
-    POSITIONAL+=("$1") # save it in an array for later
-    shift # past argument
-    ;;
-esac
+    key="$1"
+    case $key in
+        -i|--input)
+        INPUT_FILE="$2"
+        shift # past argument
+        shift # past value
+        ;;
+        -o|--output)
+        OUTPUT_FILE="$2"
+        shift # past argument
+        shift # past value
+        ;;
+        -w|--width)
+        WIDTH="$2"
+        shift # past argument
+        shift # past value
+        ;;
+        -h|--height)
+        HEIGHT="$2"
+        shift # past argument
+        shift # past value
+        ;;
+        *)    # unknown option
+        POSITIONAL+=("$1") # save it in an array for later
+        shift # past argument
+        ;;
+    esac
 done
 set -- "${POSITIONAL[@]}" # restore positional parameters
 
