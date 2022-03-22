6873a13559f6b81aca21993bb5c10dfa17cb43a0
Alexey Pobery <poberyaa@gmail.com>
2022-03-23 00:46:59 +0300

    Add script for downlaoding audio from the youtube video and convert it to the mp3 file

---
+++ youtube_2_mp3.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if [[ $# < 1 ]]; then
+	echo "Provide YouTube link to download"
+	exit 1
+fi
+
+INPUT="input.m4a"
+OUTPUT="output.mp3"
+
+if [[ $# > 1 ]]; then
+	OUTPUT=$2
+fi
+
+yt-dlp -f 140 $1 -o $INPUT
+ffmpeg -i $INPUT -c:v copy -c:a libmp3lame -q:a 4 $OUTPUT
+rm $INPUT
