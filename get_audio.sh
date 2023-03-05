6e89502ec61d2f47d1e1d85e4caa7987194a76f4
Alexey Pobery <poberyaa@gmail.com>
2023-03-05 23:51:43 +0300

    Add get_audio script

---
+++ get_audio.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+if [[ $# < 1 ]]; then
+	echo "Provide video path to get the audio"
+	exit 1
+fi
+
+INPUT="input.mp4"
+OUTPUT="output.mp3"
+
+if [[ $# > 0 ]]; then
+	INPUT=$1
+fi
+
+if [[ $# > 1 ]]; then
+	OUTPUT=$2
+fi
+
+ffmpeg -i $INPUT -c:v copy -c:a libmp3lame -q:a 4 "$OUTPUT"
