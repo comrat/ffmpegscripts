ec9bd3e98a43b3b28354938802e4f8ecfa141dd8
Alexey Pobery <poberyaa@gmail.com>
2023-06-29 12:33:44 +0300

    Add frames and time delay parameters for the video2gif script

--- video2gif.sh
+++ video2gif.sh
@@ -1,18 +1,46 @@
 #! /bin/bash
 
-if [[ $# < 1 ]]; then
-	echo "Provide at least one parameter: input file"
-	exit 2
-fi
-
 INPUT_FILE=$1
 OUTPUT_FILE="output.gif"
+FRAMES=5
+TIME_DELAY=10
 
-if [[ $# > 1 ]]; then
-	OUTPUT_FILE=$2
-fi
+POSITIONAL=()
+while [[ $# -gt 0 ]]
+do
+key="$1"
+
+case $key in
+    -i|--input)
+    INPUT_FILE="$2"
+    shift # past argument
+    shift # past value
+    ;;
+    -o|--output)
+    OUTPUT_FILE="$2"
+    shift # past argument
+    shift # past value
+    ;;
+    -t|--time)
+    TIME_DELAY="$2"
+    shift # past argument
+    shift # past value
+    ;;
+    -f|--frames)
+    FRAMES="$2"
+    shift # past argument
+    shift # past value
+    ;;
+    *)    # unknown option
+    POSITIONAL+=("$1") # save it in an array for later
+    shift # past argument
+    ;;
+esac
+done
+set -- "${POSITIONAL[@]}" # restore positional parameters
 
 mkdir frames
-ffmpeg -i "$INPUT_FILE" -vf scale=320:-1:flags=lanczos,fps=10 frames/ffout%03d.png
-convert -loop 0 frames/ffout*.png "$OUTPUT_FILE"
+ffmpeg -i "$INPUT_FILE" -vf scale=320:-1:flags=lanczos,fps=$FRAMES frames/ffout%03d.png
+# convert -loop 0 frames/ffout*.png "$OUTPUT_FILE"
+convert -delay $TIME_DELAY frames/ffout*.png $OUTPUT_FILE
 rm -rf frames
